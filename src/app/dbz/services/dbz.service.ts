df4bc8e9e170bdd20b6dfc25388686b205e79d9e
DESKTOP-PP0GK97\Luis <lbenitez@nogin.com>
2023-01-25 18:42:43 -0600

    final commit

---
+++ src/app/dbz/services/dbz.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from "@angular/core";
+import { Personaje } from "../interfaces/dbz.interface";
+
+
+@Injectable()
+export class DbzService{
+
+    
+  private _personajes : Personaje[] = [
+    {
+    nombre : "Goku",
+    poder : 15000
+    },
+    {
+      nombre : "Vegetta",
+      poder : 10000
+    }
+]
+
+get personajes() : Personaje[] {
+    return [...this._personajes];
+}
+    
+constructor(){
+        console.log("servicio Inicializado");
+    }
+
+agregarPersonaje( personaje :Personaje){
+    this._personajes.push(personaje);
+}
+
+}
